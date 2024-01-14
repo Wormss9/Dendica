7cfb16f179ea6337b678774bfdc9de803cabaab8
Wormss9 <Wormss9.bd@gmail.com>
2024-01-14 19:26:02 +0100

    Add logging

--- src/commands/wake.ts
+++ src/commands/wake.ts
@@ -2,7 +2,7 @@ import { InteractionResponseType } from "discord-interactions";
 import { Response } from "express";
 
 export const wake = (res: Response) => {
-    console.log(res)
+    console.log(res.req.body)
     return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
-import fetch, { ResponseInit } from 'node-fetch';
+import fetch from 'node-fetch';
 import { verifyKey } from 'discord-interactions';
 import { Request, Response } from 'express';
 import type { Command } from './commands.d.ts'
 
 export function VerifyDiscordRequest(clientKey: string) {
   return function (req: Request, res: Response, buf: Buffer, encoding: any) {
+    console.log("encoding")
+    console.log(encoding)
     const signature = req.get('X-Signature-Ed25519');
     const timestamp = req.get('X-Signature-Timestamp');
 
@@ -17,16 +19,16 @@ export function VerifyDiscordRequest(clientKey: string) {
   };
 }
 
-export async function DiscordRequest(endpoint: string, options: any) {
+export async function DiscordRequest(endpoint: string, method: string, body: string) {
   const url = 'https://discord.com/api/v10/' + endpoint;
-  if (options.body) options.body = JSON.stringify(options.body);
   const res = await fetch(url, {
     headers: {
       Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
       'Content-Type': 'application/json; charset=UTF-8',
       'User-Agent': 'DiscordBot (https://github.com/discord/discord-example-app, 1.0.0)',
     },
-    ...options
+    method,
+    body
   });
 
   if (!res.ok) {
@@ -42,7 +44,7 @@ export async function InstallGlobalCommands(appId: string, commands: Command[]) 
   const endpoint = `applications/${appId}/commands`;
 
   try {
-    await DiscordRequest(endpoint, { method: 'PUT', body: commands });
+    await DiscordRequest(endpoint, 'PUT', JSON.stringify(commands));
   } catch (err) {
     console.error(err);
   }
