9ca89f6c1c6c75f846dd17c65a22d14fc438704a
Wormss9 <Wormss9.bd@gmail.com>
2024-01-14 19:01:25 +0100

    Add logs

--- src/commands/wake.ts
+++ src/commands/wake.ts
@@ -2,6 +2,7 @@ import { InteractionResponseType } from "discord-interactions";
 import { Response } from "express";
 
 export const wake = (res: Response) => {
+    console.log(res)
     return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
