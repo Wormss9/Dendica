6d24f29066bbc844e8c3d4993a3f29ac82409621
Wormss9 <Wormss9.bd@gmail.com>
2024-01-14 21:43:45 +0100

    Add log

--- src/commands/wake.ts
+++ src/commands/wake.ts
@@ -4,7 +4,7 @@ import { Response } from "express";
 export const wake = async (res: Response) => {
     const client = new Client({ intents: [16777216] });
 
-    console.log(res.req.body.data)
+    console.log(res.req.body)
 
     // let x = await client.fetchGuildPreview()
 
